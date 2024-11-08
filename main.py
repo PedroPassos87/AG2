b491d66d7ed56c5864e89d0a4e292f0ef455c621
Aluno do Inatel <aluno@local.inatel.br>
2024-11-08 17:08:25 -0300

    reordenar as colunas

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ df = pd.read_csv('wholesale.csv', encoding="utf-8")
 #Converter os valores presentes no conjunto de dados para numeros inteiros
 df["Channel"] = df["Channel"].replace({"HoReCa": 0, "Retail": 1})
 df["Region"] = df["Region"].replace({"Lisbon": 0, "Oporto": 1, "Other": 2})
+
+#Reordenar as colunas do conjunto de dados
+
+column = list(df.columns)
+column.append(column.pop(0))
+df = df.reindex(columns= column)
